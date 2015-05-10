baa6223e1e06308252fe818da15fef00bdd19c1d
Navid Rojiani <nrojiani@gmail.com>
2015-05-09 22:48:02 -0400

    done with basic Graph ADT

--- graph-algorithms/graph.py
+++ graph-algorithms/graph.py
@@ -1,75 +1,177 @@
-class Vertex(object):
-    def __init__(self, n):
-        self.n = n
-        self.connections = {}
-        """ if connected to 3 with weight 5, and 7 with weight 2:
-        {
-            3: 5,
-            7: 2
-        }
+#
+# Graph ADT
+# @author Navid Rojiani
+#
+
+
+from enum import Enum
+
+class Graph(object):
+    def __init__(self, directed=False, weighted=False):
         """
-        self.visited = False
-        self.predecessor = None
-        self.pathCost = 0.0
-    def n(self):
-        return self.n
-    def addNeighbor(self, edge):
-        if dest in self.adj:
-            raise ValueError('Duplicate Vertex')
+        Vertices dictionary (each vertex stores its connections)
+        Key: Vertex id number (n)
+        Value: Vertex
+            {
+                0: <V_0>
+                2: <V_2>
+                3: <V_3>
+            }
+        """
+        self.vertices = {}
+        """ Simple List containing Edges """
+        self.edges = []
+
+        self.directed = directed
+        self.weighted = weighted
+    def addVertex(self, vertex):
+        id = vertex.getId()
+        if id in self.vertices:
+            raise ValueError('Vertex %d already in Graph' % (id))
         else:
-            self.connections[edge.getDest()] = edge.getWeight()
+            self.vertices[id] = vertex
+    def addVerticesFromList(self, vertices):
+        for v in vertices:
+            self.addVertex(v)
+    def addEdge(self, u_id, v_id, weight=1.0):
+        """ Make an edge connecting u and v """
+        if u_id not in self.vertices:
+            raise ValueError('Vertex %d not in Graph' % u)
+        elif v_id not in self.vertices:
+            raise ValueError('Vertex %d not in Graph' % v)
+        u,v = self.vertices[u_id],self.vertices[v_id]
 
-    def getAdjacent(self):
-        return sorted(list(self.connections.keys()))
-    def hasAdjacent(self):
-        return len(self.connections) > 0
+        if self.weighted and self.directed:
+            edge = Edge(u, v, weight, True, True)
+        elif self.weighted and not self.directed:
+            edge = Edge(u, v, weight, True, False)
+        elif not self.weighted and self.directed:
+            edge = Edge(u, v, 1.0, False, True)
+        else:
+            edge = Edge(u, v)
+        self.edges.append(edge)             # Add to Graph's edges list
+        if self.directed:                   # Add to u.connections
+            u.addConnection(edge)
+        else:                               # Add to u.connections & v.connections
+            u.addConnection(edge)
+            v.addConnection(edge)
+    def getVertex(self, v_id):
+        if v_id in self.vertices:
+            return self.vertices[v_id]
+        else:
+            return None
+    def getEdge(self, src_id, dest_id):
+        edge = None
+        for e in self.edges:
+            if e.getSource().getId() == src_id and \
+               e.getDest().getId() == dest_id:
+                edge = e
+        return edge
+    def getVertices(self):
+        return self.vertices
+    def getEdges(self):
+        return self.edges
+    def getVertexCount(self):
+        return len(self.vertices)
+    def getEdgeCount(self):
+        return len(self.edges)
+    def isDirected(self):
+        return self.directed
+    def isWeighted(self):
+        return self.weighted
+    def getDegree(self, v):
+        """ Get the number of edges incident to v """
+        degree = 0
+        for e in self.edges:
+            if v == e.getSource() or v == e.getDest(): degree += 1
+        return degree
+    def isIsolated(self, v):
+        return self.getDegree(v) == 0
+    def __str__(self):
+        s = 'GRAPH:\n'
+        s += '%s\n' % 'Weighted' if self.weighted else 'Unweighted\n'
+        s += '%s\n' % 'Directed' if self.directed else 'Undirected\n'
+        s += '===== Vertices (%d) =====\n' % self.getVertexCount()
+        for v in self.vertices:
+            s += '\t' + str(v) + '\n'
+        s += '===== Edges (%d) =====\n' % self.getEdgeCount()
+        for e in self.edges:
+            s += '\t' + str(e) + '\n'
+        return s
 
-    """ Marking Visitation """
-    def visit(self):
-        self.visited = True
-    def unvisit(self):
-        self.visited = False
-    def isVisited(self):
-        return self.visited
 
-    """ Marking Predecessors """
-    # def setPredecessor(self, predecessor):
-    #     self.predecessor = predecessor
-    # def getPredecessor(self):
-    #     return self.predecessor
-    # def hasPredecessor(self):
-    #     return self.predecessor != None
 
-    """ Marking Path Cost """
-    def setPathCost(self, cost):
-        self.pathCost = cost
-    def getPathCost(self):
-        return self.pathCost
+#----------------------------------------------------------------------------#
 
-    def neighborsString(self):
-        s = 'V<' + self.n + '> Neighbors: [ '
-        for adj in connections:
+class Vertex(object):
+    def __init__(self, id):
+        self.id = int(id)          # positive integer ID
+        """ Dictionary storing details of all edges incident to this vertex.
+        Key: id # of opposite vertex
+        Value: weight of edge
+        {
+            3: 5.0,
+            7: 2.0
+        }
+        - In unweighted graphs, all edges represented as having a weight of 1.0
+        - In undirected graphs, only 1 edge is stored in the Graph class for an
+            undirected edge (u,v), but both u and v will have the opposite
+            vertex as an entry in their connections dictionary:
+                u.connections => { ... v: 1.0, ... } and
+                v.connections => { ... u: 1.0, ... }.
+            For a directed graph, the edge will only be included in the source
+            vertex's connections dictionary.
+        """
+        self.connections = {}
+        self.visitStatus = VisitStatus.UNDISCOVERED
+    def getId(self):
+        """ Identifying number - must be positive integer """
+        return self.id
+    def addConnection(self, edge):
+        self.connections[edge.getOppositeVertex(self).getId()] = edge.getWeight()
+    # Visitation
+    def getVisitStatus(self):
+        return self.visitStatus
+    def setVisitStatus(self, status):
+        self.visitStatus = status
+    def isUnvisited(self):
+        return self.visitStatus == VisitStatus.UNDISCOVERED
+    def connectionsString(self):
+        s = 'V<' + str(self.id) + '> Connections: [ '
+        for adj in self.connections:
             s += str(adj) + ' '
         s += ']'
         return s
-    def connectionsString(self):
-        s = 'V<' + self.n + '> Connections: [ '
-        for adj in list(connections.keys()):
-            s += str(adj) + ' '
-        s += ']'
+    def outgoingEdgesString(self):
+        """ Return a string representation of all outgoing edges """
+        s = 'V<' + self.id + '> Connections: [ '
+        for i in range(list(self.connections.keys())):
+            s += str(self.connections.keys()[i])
+            s += ' <W = %.2f>,' % self.connections[i]
+        s = s[:-1] + ' ]'
         return s
     def __str__(self):
-        return 'V<%d>' % self.n
+        return 'V<%d>' % self.id
+
+class VisitStatus(Enum):
+    # As defined in CLRS
+    UNDISCOVERED  = 'White'
+    DISCOVERED    = 'Gray'          # Frontier, not all neighbors discovered
+    EXPLORED      = 'Black'         # All neighbors discovered
 
 
-
+#----------------------------------------------------------------------------#
 class Edge(object):
-    def __init__(self, src, dest, weight = 1.0, directed = False):
+    def __init__(self, src, dest, weight= 1.0, weighted=False, directed=False):
         """Assumes src and dest are vertices"""
         self.src = src
         self.dest = dest
-        self.weight = weight
-        self.directed = directed
+        self.directed = True if directed else False
+        self.weighted = True if weighted else False
+        if not weight:
+            self.weight = 1.0
+        else:
+            self.weight = weight        # 1.0 if unweighted
     def getSource(self):
         return self.src
     def getDest(self):
@@ -78,41 +180,22 @@ class Edge(object):
         return self.weight
     def isDirected(self):
         return self.directed
+    def isWeighted(self):
+        return self.weighted
+    def getEndpoints(self):
+        """Return (u,v) tuple for vertices u and v."""
+        return (self.src, self.dest)
+    def getOppositeVertex(self, v):
+        """Return the vertex that is opposite v on this edge."""
+        if not isinstance(v, Vertex):
+            raise TypeError('v must be an instance of class Vertex')
+        return self.dest if v is self.src else self.src
     def __str__(self):
-        return '(%d, %d, [%.2f])' % (self.src.n(), self.dest.n(), self.weight)
-
-
-class Graph(object):
-    def __init__(self):
-        self.adjLists = {} # key:V<num>, value: V<num>.adjList
-        """ 
-        Adjacency List of an undirected Graph:
-            { 
-                0: [1, 2],
-                1: [0],
-                2: [0, 3],
-                3: [2]
-            }
-        """
-    def addVertex(self, number):
-        if number in adjLists:
-            raise ValueError('Vertex %d already in Graph' % (number))
+        if self.isDirected() and self.isWeighted():
+            return '( %d ==[%.2f]==> %d )' % (self.src.getId(),self.getWeight(), self.dest.getId())
+        elif self.isDirected() and not self.isWeighted():
+            return '( %d ====> %d )' % (self.src.getId(), self.dest.getId())
+        elif not self.isDirected() and self.isWeighted():
+            return '( %d <==[%.2f]==> %d )' % (self.src.getId(),self.getWeight(), self.dest.getId())
         else:
-            adjLists[number] = []
-    def addEdge(self, edge):
-        u = edge.getSource()
-        v = edge.getDest()
-        if vertices[u] == None:
-            raise ValueError('Vertex %d not in Graph' % u)
-        elif vertices[v] == None:
-            raise ValueError('Vertex %d not in Graph' % v)
-        else:
-            if directed:
-                self.adjLists[u].append(v)
-            else:
-                self.adjLists[u].append(v)
-                self.adjLists[v].append(u)
-    def numVertices(self):
-        return len(adjLists)
-    
-
+            return '( %d <====> %d )' % (self.src.getId(), self.dest.getId())
---
+++ graph-algorithms/test_graph.py
@@ -0,0 +1,144 @@
+import unittest
+from graph import *
+
+class TestGraph(unittest.TestCase):
+    """Tests for 'graph.py'."""
+
+    def setUp(self):
+        # Undirected, Unweighted
+        self.g1 = Graph()
+        self.g1.addVertex(Vertex(1))
+        self.g1.addVertex(Vertex(2))
+        self.g1.addVertex(Vertex(3))
+        self.g1.addVertex(Vertex(4))
+        self.g1.addVertex(Vertex(5))
+        self.g1.addEdge(1,2)
+        self.g1.addEdge(1,5)
+        self.g1.addEdge(2,3)
+        self.g1.addEdge(2,4)
+        self.g1.addEdge(2,5)
+        self.g1.addEdge(3,4)
+
+        self.ud_uw_edge = self.g1.getEdge(1,2) 
+
+        # Undirected, Weighted Graph
+        self.g2 = Graph(False, True)
+        self.g2.addVertex(Vertex(1))
+        self.g2.addVertex(Vertex(2))
+        self.g2.addVertex(Vertex(3))
+        self.g2.addVertex(Vertex(4))
+        self.g2.addVertex(Vertex(5))
+        self.g2.addEdge(1,2,20)
+        self.g2.addEdge(1,5,21)
+        self.g2.addEdge(2,3,22)
+        self.g2.addEdge(2,4,23)
+        self.g2.addEdge(2,5,24)
+        self.g2.addEdge(3,4,25)
+
+        self.ud_w_edge = self.g2.getEdge(1,2) 
+
+        # Directed, Unweighted Graph
+        self.g3 = Graph(True, False)
+        self.g3.addVertex(Vertex(1))
+        self.g3.addVertex(Vertex(2))
+        self.g3.addVertex(Vertex(3))
+        self.g3.addVertex(Vertex(4))
+        self.g3.addVertex(Vertex(5))
+        self.g3.addEdge(1,2)
+        self.g3.addEdge(2,3)
+        self.g3.addEdge(2,4)
+        self.g3.addEdge(2,5)
+        self.g3.addEdge(3,4)        
+        self.g3.addEdge(4,2)        
+        self.g3.addEdge(5,1)
+        self.g3.addEdge(5,2)      
+
+        self.d_uw_edge = self.g3.getEdge(1,2)
+
+        # Directed, Weighted Graph
+        self.g4 = Graph(True, True)
+        self.g4.addVertex(Vertex(1))
+        self.g4.addVertex(Vertex(2))
+        self.g4.addVertex(Vertex(3))
+        self.g4.addVertex(Vertex(4))
+        self.g4.addVertex(Vertex(5))
+        self.g4.addEdge(1,2,10)
+        self.g4.addEdge(2,3,11)
+        self.g4.addEdge(2,4,12)
+        self.g4.addEdge(2,5,13)
+        self.g4.addEdge(3,4,14)        
+        self.g4.addEdge(4,2,15)        
+        self.g4.addEdge(5,1,16)
+        self.g4.addEdge(5,2,17)        
+
+        self.d_w_edge = self.g4.getEdge(1,2)
+
+    def tearDown(self):
+        pass
+        # self.g1 = None
+        # self.g2 = None
+        # self.g3 = None
+        # self.g4 = None
+
+        # self.ud_uw_edge = None
+        # self.ud_w_edge = None
+        # self.d_uw_edge = None
+        # self.d_w_edge = None
+
+    def testIsDirected(self):
+        self.assertFalse(self.g1.isDirected())
+        self.assertFalse(self.g2.isDirected())
+        self.assertTrue(self.g3.isDirected())
+        self.assertTrue(self.g4.isDirected())
+    
+    def testIsWeighted(self):
+        self.assertFalse(self.g1.isWeighted())
+        self.assertTrue(self.g2.isWeighted())
+        self.assertFalse(self.g3.isWeighted())        
+        self.assertTrue(self.g4.isWeighted())        
+
+    def testGetEdgeCount(self):
+        self.assertEqual(self.g1.getEdgeCount(), 6)
+        self.assertEqual(self.g2.getEdgeCount(), 6)
+        self.assertEqual(self.g3.getEdgeCount(), 8)
+        self.assertEqual(self.g4.getEdgeCount(), 8)
+
+    def testGetVertexCount(self):
+        self.assertEqual(self.g1.getVertexCount(), 5)
+        self.assertEqual(self.g2.getVertexCount(), 5)
+        self.assertEqual(self.g3.getVertexCount(), 5)
+        self.assertEqual(self.g4.getVertexCount(), 5)
+
+    def testEdge(self):
+        # also tests G.addEdge(), G.getEdge()
+        self.assertFalse(self.ud_uw_edge.isWeighted())
+        self.assertFalse(self.ud_uw_edge.isDirected())
+
+        self.assertTrue(self.ud_w_edge.isWeighted())
+        self.assertFalse(self.ud_w_edge.isDirected())
+
+        self.assertFalse(self.d_uw_edge.isWeighted())
+        self.assertTrue(self.d_uw_edge.isDirected())
+
+        self.assertTrue(self.d_w_edge.isWeighted())
+        self.assertTrue(self.d_w_edge.isDirected())
+
+    def testGraphSetup(self):
+        print(self.g1)
+        print(self.g2)
+        print(self.g3)
+        print(self.g4)
+
+        g5 = Graph()
+        vertices = []
+        for i in range(11):
+            vertices.append(Vertex(i))
+        g5.addVerticesFromList(vertices)
+        print(g5)
+
+if __name__=='__main__':
+    try:
+        unittest.main()
+    except SystemExit as inst:
+        if inst.args[0] is True: # raised by sys.exit(True) when tests failed
+            raise
